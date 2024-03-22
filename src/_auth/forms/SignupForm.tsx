c149789d1d2dac44974c2a70092b0956e4686617
Hani Saravanan <hanisaravanan@gmail.com>
2024-03-22 18:29:23 -0400

    Adding toaster shadcn

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -16,12 +16,20 @@ import { Button } from "../../components/ui/button";
 import { useForm } from "react-hook-form";
 import { Signupvalidation } from "@/lib/validation";
 import { Loader } from "lucide-react";
-import { createUserAccount } from "@/lib/appwrite/api";
 import { useToast } from "@/components/ui/use-toast";
+import {
+  useCreateUserAccount,
+  useSignInAccount,
+} from "@/lib/react-query/queriesAndMutations";
 
 const SignupForm = () => {
   const { toast } = useToast();
-  const isLoading = false;
+
+  const { mutateAsync: createUserAccount, isLoading: isCreatingUser } =
+    useCreateUserAccount();
+
+  const { mutateAsync: signInAccount, isLoading: isSingingIn } =
+    useSignInAccount();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof Signupvalidation>>({
@@ -42,11 +50,17 @@ const SignupForm = () => {
     if (!newUser) {
       return toast({ title: "Sign up failed. Please try again." });
     }
-     // const seesion = await signInAccount()
+    const session = await signInAccount({
+      email: values.email,
+      password: values.password,
+    });
+    if (!session) {
+      return toast({ title: "Sign in failed. Please try again." });
+    }
+
     
   }
 
- 
   return (
     <Form {...form}>
       <div className="sm:x-420 flex-center flex-col">
@@ -115,7 +129,7 @@ const SignupForm = () => {
             )}
           />
           <Button type="submit" className="shad-button_primary">
-            {isLoading ? (
+            {isCreatingUser ? (
               <div className="flex-center gap-2">
                 <Loader />
                 Loading...
