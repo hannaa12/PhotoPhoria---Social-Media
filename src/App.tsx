855bca605ecc55a09717aaeeac073d3072a9bc29
Hani Saravanan <hanisaravanan@gmail.com>
2024-03-21 21:33:00 -0400

    sign up form

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
 import { Routes, Route } from "react-router-dom";
-import './globals.css';
+import "./globals.css";
 import SigninForm from "./_auth/forms/SigninForm";
 import { Home } from "./_root/pages";
 import SignupForm from "./_auth/forms/SignupForm";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
+import { Toaster } from "@/components/ui/toaster";
 
 const App = () => {
   return (
     <main className="flex h-screen">
-        <Routes>
-            {/* {public} */}
-            <Route element = {<AuthLayout />}>
-                <Route path="/sign-in" element={<SigninForm />} />
-                <Route path="/sign-up" element={<SignupForm />} />
-            </Route>
+      <Routes>
+        {/* {public} */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
 
-            {/* {private} */}
-            <Route element = { <RootLayout />}>
-                <Route index element={<Home />} />
-            </Route>
-        </Routes>
+        {/* {private} */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+
+      <Toaster />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className="flex-center w-full">
+        <img src="public/assets/icons/loader.svg" alt="loader" width={24} height={24}></img>
+    </div>
+  )
+}
+
+export default Loader
